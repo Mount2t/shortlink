f59fbc80ae01bf8a96d03e9e04e836a985fcce1d
Mount <1647423208@qq.com>
2025-02-11 19:45:13 +0800

    feature:开发短链接后管系统用户注册相关功能

--- admin/src/main/java/com/nageoffer/shortlink/admin/service/UserService.java
+++ admin/src/main/java/com/nageoffer/shortlink/admin/service/UserService.java
@@ -2,6 +2,7 @@ package com.nageoffer.shortlink.admin.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.nageoffer.shortlink.admin.dao.entity.UserDO;
+import com.nageoffer.shortlink.admin.dto.req.UserRegisterReqDTO;
 import com.nageoffer.shortlink.admin.dto.resp.UserRespDTO;
 
 /**
@@ -23,4 +24,11 @@ public interface UserService extends IService<UserDO> {
      * @return  用户名存在返 True, 不存在返回 False
      */
     Boolean hasUsername(String username);
+
+    /**
+     * 注册用户
+     *
+     * @param requestParam 注册用户请求参数
+     */
+    void  register(UserRegisterReqDTO requestParam);
 }
