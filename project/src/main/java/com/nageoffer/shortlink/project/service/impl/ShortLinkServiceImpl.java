781c13d852970c80c268cc7b4105c75b3b70db1f
Mount <1647423208@qq.com>
2025-02-15 21:39:52 +0800

    feature:开发短链接项目新增短链接功能并增强

---
+++ project/src/main/java/com/nageoffer/shortlink/project/service/impl/ShortLinkServiceImpl.java
@@ -0,0 +1,88 @@
+package com.nageoffer.shortlink.project.service.impl;
+
+import cn.hutool.core.text.StrBuilder;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.nageoffer.shortlink.project.common.convention.exception.ServiceException;
+import com.nageoffer.shortlink.project.dao.entity.ShortLinkDO;
+import com.nageoffer.shortlink.project.dao.mapper.ShortLinkMapper;
+import com.nageoffer.shortlink.project.dto.req.ShortLinkCreateReqDTO;
+import com.nageoffer.shortlink.project.dto.resp.ShortLinkCreateRespDTO;
+import com.nageoffer.shortlink.project.service.ShortLinkService;
+import com.nageoffer.shortlink.project.toolkit.HashUtil;
+import groovy.util.logging.Slf4j;
+import lombok.RequiredArgsConstructor;
+import org.redisson.api.RBloomFilter;
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.stereotype.Service;
+
+/**
+ * 短链接接口实现层
+ */
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class ShortLinkServiceImpl extends ServiceImpl<ShortLinkMapper, ShortLinkDO> implements ShortLinkService {
+
+    private final RBloomFilter<String> shortUriCreateRegisterCachePenetrationBloomFilter;
+
+    @Override
+    public ShortLinkCreateRespDTO createShortLink(ShortLinkCreateReqDTO requestParam) {
+        String shortLinkSuffix = generateSuffix(requestParam);
+        String fullShortUrl = StrBuilder.create(requestParam.getDomain())
+                .append("/")
+                .append(shortLinkSuffix)
+                .toString();
+        ShortLinkDO shortLinkDO = ShortLinkDO.builder()
+                .domain(requestParam.getDomain())
+                .originUrl(requestParam.getOriginUrl())
+                .gid(requestParam.getGid())
+                .createdType(requestParam.getCreatedType())
+                .validDateType(requestParam.getValidDateType())
+                .validDate(requestParam.getValidDate())
+                .describe(requestParam.getDescribe())
+                .shortUri(shortLinkSuffix)
+                .enableStatus(0)
+                .fullShortUrl(fullShortUrl)
+                .build();
+        try {
+            baseMapper.insert(shortLinkDO);
+        } catch (DuplicateKeyException ex){
+            LambdaQueryWrapper<ShortLinkDO> queryWrapper = Wrappers.lambdaQuery(ShortLinkDO.class)
+                    .eq(ShortLinkDO::getFullShortUrl, fullShortUrl);
+            ShortLinkDO hsaShortLinkDO = baseMapper.selectOne(queryWrapper);
+            if (hsaShortLinkDO != null){
+                String message = "短链接: " + fullShortUrl + " 重复入库";
+                log.warn(message);
+                throw new ServiceException("短链接生成重复");
+            }
+        }
+        shortUriCreateRegisterCachePenetrationBloomFilter.add(shortLinkSuffix);
+        return ShortLinkCreateRespDTO.builder()
+                .fullShortUrl(shortLinkDO.getFullShortUrl())
+                .originUrl(requestParam.getOriginUrl())
+                .gid(requestParam.getGid())
+                .build();
+    }
+
+    private  String generateSuffix(ShortLinkCreateReqDTO requestParam){
+        int customGenerateCount = 0;
+        String shortUri;
+        while (true){
+            if (customGenerateCount > 10)
+            {
+                throw new ServiceException("短链接频繁生成，请稍后再试");
+            }
+            String originUrl = requestParam.getOriginUrl();
+            originUrl +=System.currentTimeMillis();
+            shortUri = HashUtil.hashToBase62(originUrl);
+            if (!shortUriCreateRegisterCachePenetrationBloomFilter.contains(requestParam.getDomain() + "/" + shortUri)){
+                break;
+            }
+            customGenerateCount++;
+        }
+        return  shortUri;
+    }
+}
