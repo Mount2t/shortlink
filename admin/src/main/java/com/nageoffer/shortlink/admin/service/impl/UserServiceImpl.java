9868c8f354a4a187ef70bd08cec9a65a31b5bbeb
Mount <1647423208@qq.com>
2025-02-28 15:19:36 +0800

    refactor:用户已登录状态如果重复登录返回Token

--- admin/src/main/java/com/nageoffer/shortlink/admin/service/impl/UserServiceImpl.java
+++ admin/src/main/java/com/nageoffer/shortlink/admin/service/impl/UserServiceImpl.java
@@ -2,6 +2,7 @@ package com.nageoffer.shortlink.admin.service.impl;
 
 
 import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.collection.CollUtil;
 import com.alibaba.fastjson2.JSON;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
@@ -108,9 +109,13 @@ public class UserServiceImpl extends ServiceImpl<UserMapper,UserDO> implements U
         if (userDO == null){
             throw new ClientException("用户不存在");
         }
-        Boolean hasLogin = stringRedisTemplate.hasKey("login_" + requestParam.getUsername());
-        if (hasLogin != null && hasLogin){
-            throw new ClientException("用户已登录");
+        Map<Object,Object> hasLoginMap = stringRedisTemplate.opsForHash().entries("login_" + requestParam.getUsername());
+        if (CollUtil.isNotEmpty(hasLoginMap)){
+            String token= hasLoginMap.keySet()
+                    .stream().findFirst()
+                    .map(Object::toString)
+                    .orElseThrow(() -> new ClientException("用户登录错误"));
+            return new UserLoginRespDTO(token);
         }
         /**
          * Hash
