ba0f74fcc8d5e1c7027a636b6ebbf24e1482c8fd
Mount <1647423208@qq.com>
2025-02-28 15:33:48 +0800

    optimise:优化单个用户创建分组数量不能大于20

--- admin/src/main/java/com/nageoffer/shortlink/admin/common/constant/RedisCacheConstant.java
+++ admin/src/main/java/com/nageoffer/shortlink/admin/common/constant/RedisCacheConstant.java
@@ -5,5 +5,13 @@ package com.nageoffer.shortlink.admin.common.constant;
  */
 public class RedisCacheConstant {
 
+    /**
+     * 用户注册分布式锁
+     */
     public static final String LOCK_USER_REGISTER_KEY = "short-link:lock_user_register:";
+
+    /**
+     * 分组创建分布式锁
+     */
+    public static final String LOCK_GROUP_CREATE_KEY = "short-link:lock_group_create:%s";
 }
