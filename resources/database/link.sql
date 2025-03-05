70f46c483238f0b456aef7fc7a862883e472f507
Mount <1647423208@qq.com>
2025-03-05 22:11:43 +0800

    fix: 修复分组唯一表索引错误

--- resources/database/link.sql
+++ resources/database/link.sql
@@ -222,6 +222,14 @@ CREATE TABLE `t_group_9`
     UNIQUE KEY `idx_unique_username_gid` (`gid`,`username`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+CREATE TABLE `t_group_unique`
+(
+    `id`  bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'ID',
+    `gid` varchar(32) DEFAULT NULL COMMENT '分组标识',
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `idx_unique_gid` (`gid`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
 CREATE TABLE `t_link_0`
 (
     `id`              bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'ID',
